22396bcf6c1f701b4b0817653758adadca9034ce
anadin1001 <114912494+anadin1001@users.noreply.github.com>
2023-05-30 16:58:29 +0300

    Add files via upload

---
+++ Seminar13/src/cts/teste_mock/SuitaCustom.java
@@ -0,0 +1,11 @@
+package cts.teste_mock;
+
+import org.junit.experimental.categories.Categories;
+import org.junit.runner.RunWith;
+
+@RunWith(Categories.class)
+@org.junit.runners.Suite.SuiteClasses(PachetTuristicTest.class)
+
+public class SuitaCustom {
+
+}
